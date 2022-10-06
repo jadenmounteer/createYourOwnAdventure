2cb54eb59077659a8ac8567957f96f634508a6c5
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-10-06 08:18:31 +0100

    Configure linter

--- src/app/confirm-modal/confirm-modal.component.ts
+++ src/app/confirm-modal/confirm-modal.component.ts
@@ -1,31 +1,38 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-confirm-modal',
   templateUrl: './confirm-modal.component.html',
-  styleUrls: ['./confirm-modal.component.scss']
+  styleUrls: ['./confirm-modal.component.scss'],
 })
-export class ConfirmModalComponent implements OnInit {
+export class ConfirmModalComponent {
   closeResult = '';
   @Input() message!: string;
   @Input() title!: string;
   @ViewChild('content') content!: any;
   @Output() confirmed = new EventEmitter();
 
-
   constructor(private modalService: NgbModal) {}
-
-  ngOnInit(): void {
-  }
 
   open() {
     const content = this.content;
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason: any) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason: any) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
@@ -42,5 +49,4 @@ export class ConfirmModalComponent implements OnInit {
     this.confirmed.emit();
     modal.close();
   }
-
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,37 +2,34 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 import { Story } from '../types/story.type';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public yourStories: Array<Story> = [];
   closeResult: string | undefined;
   @ViewChild('confirmModal') confirmModal!: ConfirmModalComponent;
   public storyToDelete!: Story;
-  public confirmModalMessage: string = "";
+  public confirmModalMessage: string = '';
 
   constructor() {}
 
   ngOnInit(): void {
     // Add stories
     const newStory: Story = {
-      name: "A tale of two cities",
-      description: "A story by Charles Dickens"
+      name: 'A tale of two cities',
+      description: 'A story by Charles Dickens',
     };
 
     const anotherStory: Story = {
-      name: "Twenty Thousand Leagues Under the Sea",
-      description: "Follow the adventures of Cpt. Nemo under the sea."
+      name: 'Twenty Thousand Leagues Under the Sea',
+      description: 'Follow the adventures of Cpt. Nemo under the sea.',
     };
 
     this.yourStories.push(newStory);
     this.yourStories.push(anotherStory);
-
   }
 
   public onDeleteStory(story: Story) {
@@ -42,8 +39,8 @@ export class HomeComponent implements OnInit {
   }
 
   public editStory(story: Story) {
-    // TOOD finish this method
-    console.log("Editing story");
+    // TODO finish this method
+    console.log('Editing story');
     const indexOfStory = this.yourStories.indexOf(story);
     console.log(indexOfStory);
   }
@@ -52,16 +49,16 @@ export class HomeComponent implements OnInit {
     // TOOD delete from the db as well.
     // Make a modal appear too to confirm
     const indexOfStory = this.yourStories.indexOf(this.storyToDelete);
-    if (indexOfStory > -1) { // only splice array when item is found
+    if (indexOfStory > -1) {
+      // only splice array when item is found
       this.yourStories.splice(indexOfStory, 1); // 2nd parameter means remove one item only
     }
   }
 
   public readStory(story: Story) {
     // TODO finish this method
-    console.log("Reading story");
+    console.log('Reading story');
     const indexOfStory = this.yourStories.indexOf(story);
     console.log(indexOfStory);
   }
-
 }
