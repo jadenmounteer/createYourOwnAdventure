fb306291e12e318c251c7e6c2f9d53190d1e492a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-10-11 22:25:38 +0100

    Finish dummy data rxjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 import { Story } from '../types/types';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-home',
@@ -18,67 +19,15 @@ export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // Add stories
-    const newStory: Story = {
-      id: 1,
-      title: 'Journy Under the Sea',
-      description:
-        'Jouney under the sea in this thrilling choose your own adventure book.',
-      draft: false,
-      pages: [
-        {
-          id: 1,
-          pageNumber: 1,
-          pageText: 'Hello world',
-          whenReaderFinishesPage: 1,
-          choices: [
-            {
-              id: 1,
-              pageNumber: 1,
-              text: 'This is a choice',
-              linksToPage: 2,
-            },
-          ],
-          ending: 'The end',
-        },
-      ],
-    };
+    // HTTP Request to get our stories
+    const ajax$ = ajax.getJSON<Array<Story>>('assets/json/dummy-data.json');
 
-    // const anotherStory: Story = {
-    //   id: 2,
-    //   title: 'The Abominable Snowman',
-    //   description:
-    //     'Test yourself in this adventure with the abominable snowman.',
-    //   draft: true,
-    // };
-
-    // const yetAnotherStory: Story = {
-    //   id: 3,
-    //   title: 'Space And Beyond',
-    //   description: 'Where will you go in Space and Beyond?',
-    //   draft: false,
-    // };
-
-    // const yesAnotherStory: Story = {
-    //   id: 4,
-    //   title: 'The Mystery of the Maya',
-    //   description: 'Discover the mysteries of this ancient people.',
-    //   draft: false,
-    // };
-
-    // const youGuessedIt: Story = {
-    //   id: 5,
-    //   title: 'The Cave of Time',
-    //   description:
-    //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at augue augue. In in enim vel neque luctus tristique. Pellentesque eleifend tristique arcu, eu eleifend tortor aliquam lacinia. Fusce venenatis id tellus ut aliquet. Vivamus nec quam sit amet purus consequat lobortis. Cras sit amet tristique nisl. Cras erat dui, cursus vitae metus nec, ullamcorper eleifend enim. Nulla finibus ac leo in porta.',
-    //   draft: false,
-    // };
-
-    this.yourStories.push(newStory);
-    // this.yourStories.push(anotherStory);
-    // this.yourStories.push(yetAnotherStory);
-    // this.yourStories.push(yesAnotherStory);
-    // this.yourStories.push(youGuessedIt);
+    ajax$.subscribe({
+      next: (stories) => {
+        this.yourStories = stories;
+      },
+      error: (err) => console.log(err),
+    });
   }
 
   public onDeleteStory(story: Story) {
