77f6c605909dbc455a0676a7ae9ac8c4b46a26c8
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-10-11 13:39:59 +0100

    More code cleanup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 import { Story } from '../types/story.type';
