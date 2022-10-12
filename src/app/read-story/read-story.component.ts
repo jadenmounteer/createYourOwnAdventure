422179499efd24e8f76c5b06f2cf6f86d8013052
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-10-12 13:44:23 +0100

    Add some not so dry code

--- src/app/read-story/read-story.component.ts
+++ src/app/read-story/read-story.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { AjaxHelperService } from '../services/ajax-helper.service';
+import { Story } from '../types/types';
 
 @Component({
   selector: 'app-read-story',
   templateUrl: './read-story.component.html',
-  styleUrls: ['./read-story.component.scss']
+  styleUrls: ['./read-story.component.scss'],
 })
 export class ReadStoryComponent implements OnInit {
+  public story: Story | undefined;
+  public storyID: number | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private ajaxHelper: AjaxHelperService
+  ) {}
 
   ngOnInit(): void {
-  }
+    // Get the story ID if we are editing
+    this.route.params.subscribe((params: Params) => {
+      this.storyID = params['id'];
+    });
 
+    const storiesObservable = this.ajaxHelper.initializeDummyData();
+
+    storiesObservable.subscribe({
+      next: (stories) => {
+        stories.forEach((story) => {
+          if (story.id === this.storyID) {
+            this.story = story;
+          }
+        });
+      },
+      error: (err) => console.log(err),
+    });
+  }
 }
