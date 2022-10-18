1f01451570075aa88961b6f91ad1b2db0ec224bb
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-10-18 08:04:42 +0100

    Begin making form work

--- src/app/create-or-edit-story/create-or-edit-story.component.ts
+++ src/app/create-or-edit-story/create-or-edit-story.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { StoriesService } from '../services/stories/stories.service';
+import { Story } from '../types/types';
 declare var $: any; // For Jquery
 
 @Component({
   selector: 'app-create-or-edit-story',
   templateUrl: './create-or-edit-story.component.html',
   styleUrls: ['./create-or-edit-story.component.scss'],
+  providers: [StoriesService],
 })
 export class CreateOrEditStoryComponent implements OnInit {
   public editMode: boolean = false;
+  public subscription: Subscription | undefined;
+  public editedItemIndex: number | undefined;
+  public editedItem?: Story;
+  @ViewChild('form') form?: NgForm;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private storiesService: StoriesService
+  ) {
     // Initialize the tooltips using jquery. Not sure why, but we have to do this.
     $(function () {
       $('[data-toggle="tooltip"]').tooltip();
@@ -32,15 +43,32 @@ export class CreateOrEditStoryComponent implements OnInit {
 
   private enableEditMode() {
     if (this.storyID) {
-      this.editMode = true;
+      this.subscription = this.storiesService.startedEditing.subscribe(
+        (index: number) => {
+          this.editedItemIndex = index;
+          this.editMode = true;
+          this.editedItem = this.storiesService.getStory(index);
+          this.form?.setValue({
+            title: this.editedItem.title,
+          });
+        }
+      );
     }
   }
 
-  public onPublish(form: NgForm) {
+  public onSubmit(form: NgForm) {
     const value = form.value;
     const title = value.title;
     console.log('Submitting form for ' + title);
 
+    // const newStory = new Story(value.title);
+
+    // TODO Finish this. Watch the video #222 in the course.
+    if (this.editMode) {
+      // this.storiesService.updateStory(this.editedItemIndex, newStory);
+    } else {
+      // this.storiesService.addStory(newStory);
+    }
     // TODO Save the pages and organize them into a story
   }
 
