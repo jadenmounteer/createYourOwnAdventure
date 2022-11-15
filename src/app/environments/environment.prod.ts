af0ff9733874dd881ca3cb8d148acb9bb313e5b3
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-11-15 13:29:25 +0000

    Add environment variables

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+/** To run the production environment, run ng serve --configuration production */
+
+export const environment = {
+  production: true,
+  apiKey: 'AIzaSyDn8j4BaTdv63f1xxvIpGL37h9Q6CSgMrQ',
+};
--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface AuthResponseData {
   idToken: string;
@@ -16,7 +17,10 @@ export interface AuthResponseData {
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  private apiKey = environment.apiKey;
+  constructor(private http: HttpClient) {
+    console.log(`API Key: ${this.apiKey}`);
+  }
 
   public signup(email: string, password: string) {
     return this.http
