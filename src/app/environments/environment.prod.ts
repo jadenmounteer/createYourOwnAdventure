0b08940f66d6f4c757dc75fc2a2b83fce156a987
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-11-15 13:33:57 +0000

    Add env variables to gitignore

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-/** To run the production environment, run ng serve --configuration production */
-
-export const environment = {
-  production: true,
-  apiKey: 'AIzaSyDn8j4BaTdv63f1xxvIpGL37h9Q6CSgMrQ',
-};
