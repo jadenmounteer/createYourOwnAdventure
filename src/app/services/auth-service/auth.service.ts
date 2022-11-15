052529d6573c47a1f6c9d38315c8ae1b76f3d4d0
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-11-15 08:01:48 +0000

    Add login error handling.

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -18,7 +18,7 @@ export interface AuthResponseData {
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  signup(email: string, password: string) {
+  public signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDn8j4BaTdv63f1xxvIpGL37h9Q6CSgMrQ',
@@ -29,42 +29,56 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknown error occurred.';
-
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMessage));
-          }
-
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email already exists.';
-
-              break;
-            case 'OPERATION_NOT_ALLOWED':
-              errorMessage = 'Password sign in is disabled.';
-              break;
-
-            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-              errorMessage =
-                'You have failed to sign up too many times. Please try again later.';
-              break;
-          }
-
-          return throwError(() => new Error(errorMessage));
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
-  login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDn8j4BaTdv63f1xxvIpGL37h9Q6CSgMrQ',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+  public login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDn8j4BaTdv63f1xxvIpGL37h9Q6CSgMrQ',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred.';
+
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists.';
+
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign in is disabled.';
+        break;
+
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage =
+          'You have failed to sign up too many times. Please try again later.';
+        break;
+
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password you entered is incorrect.';
+        break;
+
+      case 'USER_DISABLED':
+        errorMessage = 'This user is disabled.';
+        break;
+    }
+
+    return throwError(() => new Error(errorMessage));
   }
 }
