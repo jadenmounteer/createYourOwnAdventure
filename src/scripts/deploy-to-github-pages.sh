051ea3ffea823b8c20e45dd962108ffa3714f23b
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-11-22 13:28:55 +0000

    Add deploy to github action

---
+++ src/scripts/deploy-to-github-pages.sh
@@ -0,0 +1,4 @@
+cd /Users/jadenmounteer/git/createYourOwnAdventure
+npm i angular-cli-ghpages --save-dev
+ng build --base-href "https://jadenmounteer.github.io/createYourOwnAdventure/"
+npx angular-cli-ghpages --dir=dist/create-your-own-adventure
